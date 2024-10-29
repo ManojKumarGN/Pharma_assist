746b33d8ef6c237f1f0e78a76391f13f408e2e10
ManojKumarGN <manojkumargn1517@gmail.com>
2024-10-29 14:12:39 +0530

    Project Started

---
+++ src/main/java/com/example/pharmassist/PharmaAssistApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pharmassist/PharmaAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
